a740761c89cb075e20ef8f835662c12df4d892a9
theglazeshelf <theglazeshelf@gmail.com>
2026-09-15 12:40:09 -0600

    Delete app/AppScreenRoute.tsx

--- app/AppScreenRoute.tsx
+++
@@ -1,6 +0,0 @@
-import GlazeShelfApp from "./GlazeShelfApp";
-import type { AppScreen } from "@/lib/app-routes";
-
-export default function AppScreenRoute({ screen }: { screen: AppScreen }) {
-  return <GlazeShelfApp initialScreen={screen} />;
-}
